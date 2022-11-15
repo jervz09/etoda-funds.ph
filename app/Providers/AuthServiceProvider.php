48094b781061e51cf57213984f76c2691bbb889a
jervz09 <jervyariola@gmail.com>
2022-11-15 23:25:18 +0800

    feat: added gate permission

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +26,15 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+
+        /* define a admin user role */
+        Gate::define('is_Admin', function($user) {
+            return $user->is_admin == 1;
+         });
+
+         /* define a user role */
+         Gate::define('is_User', function($user) {
+             return $user->is_admin == 0;
+         });
     }
 }
