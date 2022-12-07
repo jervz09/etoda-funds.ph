02159691c190273f2bfe1cdf761dff0e22d1cbaa
jervz09 <jervyariola@gmail.com>
2022-12-07 11:00:30 +0800

    restrict digit fields

--- resources/views/admin/funds/add-new.blade.php
+++ resources/views/admin/funds/add-new.blade.php
@@ -67,13 +67,16 @@
 @stop
 @section('js')
 <script>
-    $('#mobile_number').on('keypress', function (evt) {
-    // var regex = new RegExp("^[a-zA-Z0-9]+$");
-    // var key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
-    // if (!regex.test(key)) {
-    //    event.preventDefault();
-    //    return false;
-    // }
+    $('#amount').on('keypress', function (evt) {
+        evt = (evt) ? evt : window.event;
+        var charCode = (evt.which) ? evt.which : evt.keyCode;
+        if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+            return false;
+        }
+    });
+
+
+    $('#current_amount').on('keypress', function (evt) {
         evt = (evt) ? evt : window.event;
         var charCode = (evt.which) ? evt.which : evt.keyCode;
         if (charCode > 31 && (charCode < 48 || charCode > 57)) {
